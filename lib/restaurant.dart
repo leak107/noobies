a5f3e96be8355e27ca036ca5575a191e18fa1639
Yonathan <yonathansusanto100@gmail.com>
2022-05-19 03:19:53 +0700

    Update

--- lib/restaurant.dart
+++ lib/restaurant.dart
@@ -40,35 +40,30 @@ class Restaurant {
           name: restaurant['name'],
           rating: restaurant['rating'],
           description: restaurant['description'],
-          image: restaurant['image'],
+          image: restaurant['pictureId'],
           location: Location(
               address: faker.address.streetName(),
               city: restaurant['city'],
               country: faker.address.country(),
           ),
           menus: Menu(
-              foods: (restaurant['menus']['foods'] as List<dynamic>).map((food) => Item(
-                      name: food['name'],
-                      price: (10000 + Random().nextInt(50000 - 10000)).toString(),
-              )).toList(),
-              drinks: (restaurant['menus']['drinks'] as List<dynamic>).map((drink) => Item(
-                      name: drink['name'],
-                      price: (10000 + Random().nextInt(50000 - 10000)).toString(),
-              )).toList(),
+              foods: (restaurant['menus']['foods'] as List<dynamic>).map((food) => Item( name: food['name'], price: (10000 + Random().nextInt(50000 - 10000)).toString(),)).toList(),
+              drinks: (restaurant['menus']['drinks'] as List<dynamic>).map((drink) => Item( name: drink['name'], price: (10000 + Random().nextInt(50000 - 10000)).toString(),)).toList(),
           )
       );
   }
 }
 
-Future<Restaurant> fetchRestaurant() async {
+Future <List<Restaurant>> fetchRestaurant() async {
     final url = 'https://gist.githubusercontent.com/LittleFireflies/e8c08f316217b5018b76b3e5463da34d/raw/bf145725d1d9af2635b71bd6d5d9dc0b79712157/local_restaurant.json';
 
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-        return Restaurant.fromJson(jsonDecode(response.body));
+        return [
+            for (final restaurant in jsonDecode(response.body)['restaurants']) Restaurant.fromJson(restaurant)
+        ];
     } else {
-        throw Exception('Failed to laod album');
+        throw Exception('Failed to load');
     }
 }
-
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:noobies/restaurant.dart';
+import 'package:noobies/restaurant_widget.dart';
 
 class SearchPage extends StatelessWidget {
-  const SearchPage({Key? key}) : super(key: key);
+    final List<Restaurant> restaurants;
+
+  const SearchPage({Key? key, required this.restaurants}) : super(key: key);
+
+  void restaurantSearch(String query) {
+      List<Restaurant> filteredRestaurant = [];
+      List<Restaurant> temporary = restaurants;
+
+      if (query.isNotEmpty) {
+          print(query);
+          filteredRestaurant = restaurants.where((restaurant) => restaurant.name.contains(query)).toList();
+
+          restaurants.clear();
+          restaurants.addAll(filteredRestaurant);
+      } else {
+          restaurants.clear();
+          restaurants.addAll(temporary);
+      }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +46,19 @@ class SearchPage extends StatelessWidget {
                 ),
                 hintText: 'Search...',
                 border: InputBorder.none),
+            onChanged: (value) => restaurantSearch(value),
           ),
         ),
       )),
+            body: ListView.builder(
+                itemCount: restaurants.length,
+                itemBuilder: (context, index) {
+                    return RestaurantWidget(
+                        restaurant: restaurants[index]
+                    );
+                }
+
+            ),
     );
   }
 }
